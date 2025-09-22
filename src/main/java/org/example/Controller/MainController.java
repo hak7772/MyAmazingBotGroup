a20003ca9eec15f07d3d6d6f036c33ac1ea4834d
dontik863-cpu <dontik863@gmail.com>
2025-09-23 01:59:26 +0700

    Java Dmitriy

--- src/main/java/org/example/Controller/MainController.java
+++ src/main/java/org/example/Controller/MainController.java
@@ -1,10 +1,11 @@
 package org.example.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-
+    @GetMapping("/")
     public String collection() {
         return "index";
     }
